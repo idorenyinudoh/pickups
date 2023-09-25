0c85048c7a85f7b35539f0ff71e43af8cd63ca59
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-09-25 15:50:41 +0100

    fix: remove background color from embedded map container

--- components/TheLocation.vue
+++ components/TheLocation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-primary pt-[100%] relative">
+  <div class="pt-[100%] relative">
     <iframe
       class="absolute top-0 left-0 w-full h-full"
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.7105378646675!2d-73.88672452334346!3d40.68034643955179!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25db5c22cfdf3%3A0x585e814a251a384a!2s3001%20Fulton%20St%2C%20Brooklyn%2C%20NY%2011208%2C%20USA!5e0!3m2!1sen!2sng!4v1695648930710!5m2!1sen!2sng"
