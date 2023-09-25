b4bc85311e5399b8f3e9bbcf0127de6174f8e519
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-09-25 04:28:22 +0100

    feat: create component for intro section of offer

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="w-2/4 min-w-[300px] max-w-[400px] mx-auto my-16 border border-solid border-gray-300">
-    <TheLocation />
+  <main class="w-2/4 min-w-[300px] max-w-[400px] mx-auto my-16 border border-solid border-gray-300 bg-white">
+    <PickupsIntro />
   </main>
 </template>
