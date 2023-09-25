ce2fed71728af79130ce709cadc7ee1a6b26fe60
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-09-25 03:10:06 +0100

    feat: create component for map section

---
+++ components/TheLocation.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-primary pt-[100%] relative">
+    <UButton class="absolute top-[10%] left-[5%] bg-white rounded-full" icon="i-heroicons-chevron-left" size="sm" color="primary" variant="ghost" />
+  </div>
+</template>
