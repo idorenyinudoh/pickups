b37cb78aa60d5650bea66c052f5a0ed84a02de41
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-09-25 03:09:31 +0100

    build: configure app theme primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,23 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          100: '#D2E6FD',
+          200: '#B4D5FC',
+          300: '#8FC1FB',
+          400: '#69ACFA',
+          500: '#4497F8',
+          DEFAULT: '#1E82F7',
+          600: '#196CCE',
+          700: '#1457A5',
+          800: '#0F417C',
+          900: '#0A2B52',
+          950: '#061A31',
+        }
+      }
+    },
   },
   plugins: [],
 }
